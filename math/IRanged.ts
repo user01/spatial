ed33f11e3194eda320d159018cd586941f00854f
Erik <erik.langenborg@gmail.com>
2015-02-09 23:37:01 -0500

    Added missing files

---
+++ math/IRanged.ts
@@ -0,0 +1,9 @@
+/// <reference path="./vector.ts" />
+
+module Spatial {
+  export interface IRanged {
+    distanceTo(v:Vector):number;
+    intensityAt(v:Vector):number;
+    closestVector(v:Vector):Vector;
+  }
+}
