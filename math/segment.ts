eaddadd0bb12d3df56c5a63b5d599317232540af
Erik <erik.langenborg@gmail.com>
2015-02-13 01:59:30 -0500

    Adding content to segment

--- math/segment.ts
+++ math/segment.ts
@@ -1,19 +1,41 @@
 /// <reference path="../typings/node.d.ts" />
+/// <reference path="./vector.ts" />
 
 module Spatial {
-  class Segement {
-    constructor(private _dimension:number=2){
+  export class Segement {
+    
+    public get length():number {
+      return this._base.distanceTo(this._tip);
+    }
+    
+    constructor(protected _base:Vector, protected _tip:Vector){
+      if (this._base.dimension != this._tip.dimension){
+        throw new RangeException();
+      }
     }
   }
 
-  export class Segment2 {
-    constructor(){}
+  export class Segment2 extends Segement {
+    public get Base():Vector2 {
+      return <Vector2>this._base;
+    }
+    public get Tip():Vector2 {
+      return <Vector2>this._tip;
+    }
+    
+    constructor(base:Vector2,tip:Vector2){
+      super(base,tip);
+    }
   }
-  export class Segment3 {
-    constructor(){}
+  export class Segment3 extends Segement {
+    constructor(base:Vector3,tip:Vector3){
+      super(base,tip);
+    }
   }
-  export class Segment4 {
-    constructor(){}
+  export class Segment4 extends Segement {
+    constructor(base:Vector4,tip:Vector4){
+      super(base,tip);
+    }
   }
 
 }
