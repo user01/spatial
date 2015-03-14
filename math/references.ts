f7b49319af6033be2fc3177cb70da880f85dacb9
Erik <erik.langenborg@gmail.com>
2015-03-14 01:24:54 -0400

    References now call the associated classes

--- math/references.ts
+++ math/references.ts
@@ -4,4 +4,6 @@
 /// <reference path="./ISerializable.ts" />
 /// <reference path="./IEquality.ts" />
 
+/// <reference path="./ramp.ts" />
 /// <reference path="./vector.ts" />
+/// <reference path="./segment.ts" />
