15c58925b516b16accecf77ce2df256e67d310ff
Erik Langenborg <erik.langenborg@gmail.com>
2016-04-13 18:15:28 -0400

    Simplification

--- math/main.ts
+++ math/main.ts
@@ -5,7 +5,7 @@ import Vector = require("./vector");
 import Segment = require("./segment");
 
 module.exports = {
-  Ramp: Ramp,
-  Vector: Vector,
-  Segment: Segment
+  Ramp,
+  Vector,
+  Segment
 };
