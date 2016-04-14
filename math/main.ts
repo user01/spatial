240b6d19aa9de797991b60c2fb96398f9b4bb38c
Erik Langenborg <erik.langenborg@gmail.com>
2016-04-13 21:20:16 -0400

    Added interfaces to be exposed by spatial

--- math/main.ts
+++ math/main.ts
@@ -2,3 +2,6 @@
 export * from "./ramp";
 export {Vector2, Vector3, Vector4} from "./vector";
 export * from "./segment";
+export * from "./interfaces/iequality";
+export * from "./interfaces/iranged";
+export * from "./interfaces/iserializable";
