3243cfe422b8a773056dc36abfe2af150460b708
Erik Langenborg <erik.langenborg@gmail.com>
2016-04-15 16:52:10 -0400

    Added vectorbase to export

--- build/math/main.js
+++ build/math/main.js
@@ -3,9 +3,6 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 __export(require("./ramp"));
-var vector_1 = require("./vector");
-exports.Vector2 = vector_1.Vector2;
-exports.Vector3 = vector_1.Vector3;
-exports.Vector4 = vector_1.Vector4;
+__export(require("./vector"));
 __export(require("./segment"));
 //# sourceMappingURL=main.js.map
--- math/main.ts
+++ math/main.ts
@@ -1,6 +1,6 @@
 
 export * from "./ramp";
-export {Vector2, Vector3, Vector4} from "./vector";
+export * from "./vector";
 export * from "./segment";
 export * from "./interfaces/iequality";
 export * from "./interfaces/iranged";
