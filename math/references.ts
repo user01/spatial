3baa29eececc08700350691f5c0727083c1a63c8
Erik Langenborg <erik.langenborg@gmail.com>
2016-04-16 22:14:39 -0400

    Another attempt at es6 includes

--- math/references.ts
+++ math/references.ts
@@ -3,11 +3,3 @@
 /// <reference path="../typings/node.d.ts" />
 /// <reference path="../typings/moment-node.d.ts" />
 
-/// <reference path="./interfaces/iranged.ts" />
-/// <reference path="./interfaces/iserializable.ts" />
-/// <reference path="./interfaces/iequality.ts" />
-
-/// <reference path="./ramp.ts" />
-/// <reference path="./vector.ts" />
-/// <reference path="./segment.ts" />
-
